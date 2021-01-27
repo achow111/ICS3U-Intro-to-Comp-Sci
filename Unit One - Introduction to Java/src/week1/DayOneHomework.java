caa0849c0718e807ff02ff6b8c3e5b6a53a82242
Aaron Chow <achow@bayviewglen.ca>
2021-01-27 10:22:40 -0500

    Day One Homework

---
+++ Unit One - Introduction to Java/src/week1/DayOneHomework.java
@@ -0,0 +1,36 @@
+package week1;
+
+class DayOneHomework {
+    public static void main(String[] args) {
+        //This prints my initials (AC)
+        System.out.println("\n     A           CCCCCCCC  ");
+        System.out.println("    A  A        C        C ");
+        System.out.println("   A     A     C           ");
+        System.out.println("  AAAAAAAAA    C           ");
+        System.out.println(" A         A    C        C ");
+        System.out.println("A           A    CCCCCCCC  ");
+        System.out.println();
+
+        //This prints a stick figure
+        System.out.println("\t(*__*)");
+        System.out.println("\t __|__");
+        System.out.println("\t   |");
+        System.out.println("\t  / \\");
+        System.out.println();
+
+        //This prints a mailing adress
+        System.out.println(" 911 Police Road ");
+        System.out.println();
+
+        //This prints the Mercy Hospital Billing Statement
+        System.out.println("/////////////////////////////////////////////////////////////////////");
+        System.out.println("/                                                                   /");
+        System.out.println("/                    Our Lady of Mercy Hospital                     /");
+        System.out.println("/                    --------------------------                     /");
+        System.out.println("/                                                                   /");
+        System.out.println("/                        1306 Central City                          /");
+        System.out.println("/                       Phone (416) 555-1212                        /");
+        System.out.println("/                                                                   /");
+        System.out.println("/////////////////////////////////////////////////////////////////////");
+    }
+}
