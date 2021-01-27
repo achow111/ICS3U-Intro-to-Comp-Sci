54496b5c1c5c58d6a512dfc326d8b5d0f36c8bb1
Aaron Chow <achow@bayviewglen.ca>
2021-01-27 10:05:11 -0500

    Println and Escape Sequences

---
+++ Unit One - Introduction to Java/src/week1/ExampleTwo.java
@@ -0,0 +1,8 @@
+package week1;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("This is very \"exciting\" code!");
+        System.out.println("This \nis \na \nvalid \nescape \nsequence");
+    }
+}
