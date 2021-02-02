95010b407550a6bf5c5b361836457fcba1736a2b
Aaron Chow <achow@bayviewglen.ca>
2021-02-01 19:14:42 -0500

    Println simple program

--- Unit One - Introduction to Java/src/week1/ExampleOne.java
+++ Unit One - Introduction to Java/src/week1/ExampleOne.java
@@ -1,5 +1,6 @@
 package week1;
 
+//This is a comment
 //Example One is name of class; same as file
 //Classes are meant to model an objects behaviour
 //Naming convention for classes is upper case first letter for each word
